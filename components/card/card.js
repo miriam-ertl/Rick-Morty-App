adc57a8935eafeb1b0dda6011bbdd18909a461dd
Miriam Ertl <miriam_ertl@web.de>
2023-08-16 15:09:20 +0200

    imageItem

--- components/card/card.js
+++ components/card/card.js
@@ -1,4 +1,8 @@
 export function createCharacterCard(data) {
-  const cardItem = document.createElement("li");
+  const cardItem = document.createElement("ul");
   cardItem.classList.add("card");
+
+  // add HTML element <IMG>
+  const imageItem = document.createElement("li");
+  imageItem.classList.add("card__image");
 }
