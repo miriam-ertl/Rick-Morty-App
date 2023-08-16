5efcc7284d56b84e8a2f3977f357c9ffc8211a9a
Miriam Ertl <miriam_ertl@web.de>
2023-08-16 16:00:24 +0200

    add fetch function

--- components/card/card.js
+++ components/card/card.js
@@ -1,8 +1,26 @@
 export function createCharacterCard(data) {
-  const cardItem = document.createElement("ul");
-  cardItem.classList.add("card");
-
-  // add HTML element <IMG>
+  const cardContainer = document.createElement("ul");
+  cardContainer.classList.add("card-container");
+  /* // add HTML element <IMG>
   const imageItem = document.createElement("li");
-  imageItem.classList.add("card__image");
+  imageItem.classList.add("card__image"); */
+  //cardContainer.innerHTML =
+}
+console.log("Hi");
+
+document.addEventListener("scroll", async (event) => {
+  const users = await getUsers();
+  renderUsers(users);
+});
+
+function renderUsers(users) {
+  users.forEach((user) => {
+    const listItem = `<li><img src="${user.image}"> ${user.name} ${user.status} ${user.type} ${user.episode}</li>`;
+    cardContainer.innerHTML += listItem;
+  });
+}
+async function getUsers() {
+  const response = await fetch("https://rickandmortyapi.com/api/character/197");
+  const json = await response.json();
+  return json.data;
 }
