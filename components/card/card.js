76ef0d92a5f45546f8b744736c1d4bfed1030157
Miriam Ertl <miriam_ertl@web.de>
2023-08-16 17:37:47 +0200

    adjust index.js

--- components/card/card.js
+++ components/card/card.js
@@ -1,19 +1,9 @@
+const cardContainer = document.querySelector('[data-js="card-container"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+
 export function createCharacterCard(character) {
   const cardContainer = document.createElement("li");
   cardContainer.classList.add("card-container");
-  /* // add HTML element <IMG>
-  const imageItem = document.createElement("li");
-  imageItem.classList.add("card__image"); */
-  //cardContainer.innerHTML =
-}
-console.log("Hi");
-
-document.addEventListener("scroll", async (event) => {
-  const characters = await getCharacters();
-  renderCharacters(characters);
-});
-
-function renderCharacters(character) {
   characters.forEach((character) => {
     const listItem = `<li><img src="${character.image}">
     <h2 class="card__title">${character.name}</h2> 
@@ -21,10 +11,35 @@ function renderCharacters(character) {
     <p class="card__info-title">${character.type}</p>
     <p class="card__info-title">${character.episode.length}</p></li>`;
     cardContainer.innerHTML += listItem;
+    /* // add HTML element <IMG>
+  const imageItem = document.createElement("li");
+  imageItem.classList.add("card__image"); */
+    //cardContainer.innerHTML =
   });
 }
-async function getCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/");
-  const json = await response.json();
-  return json.data;
-}
+console.log("Hi");
+
+// nextButton.addEventListener("click", async (event) => {
+//   const characters = await getCharacters();
+//   renderCharacters(characters);
+//   console.log(characters);
+// });
+// console.log("HiMoin");
+
+// function renderCharacters(characters) {
+//   characters.forEach((character) => {
+//     const listItem = `<li><img src="${character.image}">
+//     <h2 class="card__title">${character.name}</h2>
+//     <p class="card__info-title">${character.status}</p>
+//     <p class="card__info-title">${character.type}</p>
+//     <p class="card__info-title">${character.episode.length}</p></li>`;
+//     cardContainer.innerHTML += listItem;
+//   });
+//}
+// async function getCharacters() {
+//   const response = await fetch("https://rickandmortyapi.com/");
+//   const json = await response.json();
+//   return json.data;
+// }
+console.log("hallo");
+//console.log("ok", response.ok, response.status);
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import { createCharacterCard } from "./components/card/card.js";
 
-const cardContainer = document.querySelector('[data-js="card-container"]');
+// const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
-const nextButton = document.querySelector('[data-js="button-next"]');
+//const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+//const createCharacterCard = createCharacterCard();
+//root.append(createCharacterCard);
+
+//function
+async function getCharacters() {
+  const response = await fetch("https://rickandmortyapi.com/");
+  const json = await response.json();
+  return json.data;
+}
