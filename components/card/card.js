eb4c5abd4f94de3cd7d9a182eefba41f833c6103
Miriam Ertl <miriam_ertl@web.de>
2023-08-17 11:07:32 +0200

    the final final fetch function

--- components/card/card.js
+++ components/card/card.js
@@ -1,14 +1,8 @@
-const cardContainer = document.querySelector('[data-js="card-container"]');
-const nextButton = document.querySelector('[data-js="button-next"]');
+export function createCharacterCard(character) {
+  const card = document.createElement("li");
+  card.classList.add("card");
 
-export function createCharacterCard(characters) {
-  const cardContainer = document.createElement("li");
-  cardContainer.classList.add("card-container");
-  cardContainer.innerHTML = "";
-  characters.forEach((character) => {
-    console.log("character.forEach", character);
-    const listItem = `<li class="card">
-    <div class="card__image-container">
+  card.innerHTML = `<div class="card__image-container">
       <img
         class="card__image"
         src="${character.image}"
@@ -26,43 +20,6 @@ export function createCharacterCard(characters) {
         <dt class="card__info-title">Occurrences</dt>
         <dd class="card__info-description">${character.episode.length}</dd>
       </dl>
-    </div>
-  </li>`;
-    // const listItem = `<li><img src="${character.image}">
-    // <h2 class="card__title">${character.name}</h2>
-    // <p class="card__info-title">${character.status}</p>
-    // <p class="card__info-title">${character.type}</p>
-    // <p class="card__info-title">${character.episode.length}</p></li>`;
-    cardContainer.innerHTML += listItem;
-    /* // add HTML element <IMG>
-  const imageItem = document.createElement("li");
-  imageItem.classList.add("card__image"); */
-    //cardContainer.innerHTML =
-  });
+    </div>`;
+  return card;
 }
-console.log("Hi");
-
-// nextButton.addEventListener("click", async (event) => {
-//   const characters = await getCharacters();
-//   renderCharacters(characters);
-//   console.log(characters);
-// });
-// console.log("HiMoin");
-
-// function renderCharacters(characters) {
-//   characters.forEach((character) => {
-//     const listItem = `<li><img src="${character.image}">
-//     <h2 class="card__title">${character.name}</h2>
-//     <p class="card__info-title">${character.status}</p>
-//     <p class="card__info-title">${character.type}</p>
-//     <p class="card__info-title">${character.episode.length}</p></li>`;
-//     cardContainer.innerHTML += listItem;
-//   });
-//}
-// async function getCharacters() {
-//   const response = await fetch("https://rickandmortyapi.com/");
-//   const json = await response.json();
-//   return json.data;
-// }
-console.log("hallo");
-//console.log("ok", response.ok, response.status);
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { createCharacterCard } from "./components/card/card.js";
 
-// const cardContainer = document.querySelector('[data-js="card-container"]');
+const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
-//const nextButton = document.querySelector('[data-js="button-next"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
@@ -15,12 +15,15 @@ const maxPage = 1;
 const page = 1;
 const searchQuery = "";
 
-//const createCharacterCard = createCharacterCard();
-//root.append(createCharacterCard);
-
-//function
+//fetch function
 async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/character");
-  const json = await response.json();
-  return json.data;
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+  const charactersData = data.results;
+  cardContainer.innerHTML = "";
+  charactersData.forEach((character) => {
+    const card = createCharacterCard(character);
+    cardContainer.append(card);
+  });
 }
+fetchCharacters();
